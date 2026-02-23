3c199f82f95d1939ac6575288a7303c922f25191
DaVid Justin <chaosdudeserad@gmail.com>
2026-02-24 05:21:17 +0800

    Refactor main logic to utilize LogicSystem for processing statements and fixed the mispelled file name TautologyChecker

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from statements import Statements
-from truth_table import TruthTable
-from tautoloy_checker import TautologyChecker
-from connectives import evaluate_expression
+from logicSystem import LogicSystem
 
 def main():
     print("=== Propositional Logic System ===")
@@ -15,29 +12,20 @@ def main():
     user_input = input("\nEnter a logical statement: ")
 
     # Create a statement object
-    state = Statements(user_input)
+    system = LogicSystem(user_input)
 
-    # generate truth table
-    tt = TruthTable(state)
-    tt.generate_table()
+    # processing statement
+    system.process()
 
     # display table
-    print("\nTruth Table:")
-    tt.display_table()
+    system.show_results()
 
-    # check if statement is tautology/contradiction/contingent
-    checker = TautologyChecker(tt)
-    result = checker.check_statement()
-    print(f"\nStatement analysis: {result}")
+    # Equivalence check
+    choice = input("\nCheck equivalence with another statement? (y/n): ")
 
-    # optional: checks if its equivalence with another statement
-    choice = input("\nDo you want to check equivalence with another statement? (y/n): ")
     if choice.lower() == "y":
         second_input = input("Enter second statement: ")
-        state2 = Statements(second_input)
-        tt2 = TruthTable(state2)
-        tt2.generate_table()
-        equivalent = TautologyChecker.check_equivalence(tt, tt2)
+        equivalent = system.check_equivalence(second_input)
         print(f"Statements are logically equivalent: {equivalent}")
 
 if __name__ == "__main__":
