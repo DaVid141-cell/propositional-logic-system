5804c5215e2909cbf6ae811a5b4d5d50bc385c06
DaVid Justin <chaosdudeserad@gmail.com>
2026-02-25 19:32:31 +0800

    Add user input handling for variable values and step-by-step evaluation in LogicSystem

--- logicSystem.py
+++ logicSystem.py
@@ -1,6 +1,8 @@
+import re
 from statements import Statements
 from truth_table import TruthTable
 from tautology_checker import TautologyChecker
+from connectives import evaluate_expression
 
 
 class LogicSystem:
@@ -34,3 +36,56 @@ class LogicSystem:
             self.truth_table, tt2
         )
         return equivalent
+    
+    def solve_given_values(self):
+        print("\nSolve Expression using given values")
+
+        values_dict = {}
+
+        # Ask user input automatically
+        for var in self.statement.variables:
+            while True:
+                val = input(f"Enter value for {var} (T/F): ").strip().upper()
+
+                if val == "T":
+                    values_dict[var] = True
+                    break
+                elif val == "F":
+                    values_dict[var] = False
+                    break
+                else:
+                    print("Invalid input. Please enter T or F")
+        
+        result = evaluate_expression(self.expression, values_dict)
+
+
+
+        return values_dict, result
+
+    def solve_with_steps(self, values_dict):
+        print("\nStep-by-step evaluation:")
+
+        expr = self.expression
+
+        while "(" in expr:
+            # find innermost parentheses
+            inner = re.findall(r'\([^()]+\)', expr)
+
+            for part in inner:
+                clean = part[1:-1]                                  # remove parentheses
+                result = evaluate_expression(clean, values_dict)    # evaluate part
+                output = "T" if result else "F"                     # for printing only
+                print(f"{part} = {output}")
+                
+                # Replace parentheses with Python boolean
+                expr = expr.replace(part, str(result), 1)
+                break                                                # solve only one parenthesis at a time
+
+        # evaluate the remaining expression
+        final_result = evaluate_expression(expr, values_dict)
+        output = "T" if final_result else "F"
+        print(f"{self.expression} = {output}")
+
+        return final_result
+
+
