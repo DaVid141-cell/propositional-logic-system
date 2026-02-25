be1587eb9df8c8b5fc6897f7dfefb62341665472
DaVid Justin <chaosdudeserad@gmail.com>
2026-02-25 19:33:34 +0800

    Fix disjunction symbol in output and improve user input handling for solving statements

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     print("\nSymbols and Discription: ")
     print("'~' = Negation/not/~p")
     print("'^' = Conjunction/and/p ^ q")
-    print("'&' = Disjunction/or/p & q")
+    print("'|' = Disjunction/or/p v q")
     print("'>' = Conditional/if p then q/p > q")
     print("'<>' = Biconditional/p if and only if q/p <> q")
     user_input = input("\nEnter a logical statement: ")
@@ -20,10 +20,28 @@ def main():
     # display table
     system.show_results()
 
-    # Equivalence check
-    choice = input("\nCheck equivalence with another statement? (y/n): ")
+    # Optional if user has a given value
+    while True:
+        solve = input("\nDo you want to solve using given values? (y/n): ")
+        if solve in ["y", "n"]:
+            break
+        print("Invalid input. Please enter 'y' or 'n'.")
 
-    if choice.lower() == "y":
+    if solve == "y":
+        values_dict, result = system.solve_given_values()            
+        system.solve_with_steps(values_dict)
+        output = "T" if result else "F"
+        print(f"\nFinal result for given values: {system.expression} = {output}")
+        
+
+    # Equivalence check
+    while True:
+        choice = input("\nCheck equivalence with another statement? (y/n): ")
+        if choice in ["y", "n"]:
+            break
+        print("Invalid input. Please enter 'y' or 'n'.")
+
+    if choice== "y":
         second_input = input("Enter second statement: ")
         equivalent = system.check_equivalence(second_input)
         print(f"Statements are logically equivalent: {equivalent}")
