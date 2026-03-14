b65bdf6d98c2b912dba43c446c4486c19b269816
DaVid Justin <chaosdudeserad@gmail.com>
2026-03-15 00:33:40 +0800

    feat: add PopUpArrow component for SVG arrow display

---
+++ frontend/src/components/ui/Pop-up-arrow.tsx
@@ -0,0 +1,17 @@
+export function PopUpArrow() {
+    return (
+        <svg className="" width="200" height="150" viewBox="0 0 100 100">
+            <path
+                d="M20 20 L50 30 L80 20
+                M25 30 L50 40 L75 30
+                " 
+                fill="none"
+                stroke="#f34f29"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    );
+}
+
