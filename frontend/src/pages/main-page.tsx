ec915cc2d0dfea8df32635ef1fc052d15496c815
DaVid Justin <chaosdudeserad@gmail.com>
2026-03-28 00:24:17 +0800

    updated the main-page into more clean readable code

--- frontend/src/pages/main-page.tsx
+++ frontend/src/pages/main-page.tsx
@@ -3,29 +3,22 @@ import { BoxHeader } from "../components/ui/box-header";
 import { BoxMain } from "../components/ui/box-main";
 import { BoxFooter } from "../components/ui/box-footer";
 import { MainLayout } from "../components/layout/MainLayout";
-import { BoxMainContentFirst } from "../components/ui/box-main-content";
+import type { SolveResult, AnalyzeResult } from "../components/ui/box-main-content-input";
+import InputForm from "../components/inputForm";
+import { useState } from "react";
+import Output from "../components/output";
 
 export default function MainPage() {
+  const [analyzeData, setAnalyzeData] = useState<AnalyzeResult | null>(null);
+  const [solveData, setSolveData] = useState<SolveResult | null>(null);
+
   return (
     <MainLayout>
 
-      <BoxFrame className="h-full justify-self-center">
-        <BoxHeader title="InputForm.md" />
-          <BoxMain className="pt-20">
-            <BoxMainContentFirst/>
-          </BoxMain>
+      <InputForm onAnalyze={setAnalyzeData} onSolve={setSolveData} />
 
-          <BoxFooter/>
-      </BoxFrame>
-
-      <BoxFrame className="h-200 justify-self-center">
-        <BoxHeader title="Truth Table" />
-          <BoxMain>
-            <div className="">
-                        
-            </div>
-          </BoxMain>
-      </BoxFrame>
+      <Output analyzeData={analyzeData} />
+      
 
       <BoxFrame className="row-span-4 h-320">
         <BoxHeader title="Notes.md" className=""/>
