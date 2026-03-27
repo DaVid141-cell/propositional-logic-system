02ba6c1699c4e37285f5c0eefdf9aa13855fc4bb
DaVid Justin <chaosdudeserad@gmail.com>
2026-03-28 00:21:10 +0800

    updated the inputForm.tsx as the main wrapper for the inputForm

--- frontend/src/components/inputForm.tsx
+++ frontend/src/components/inputForm.tsx
@@ -1,12 +1,25 @@
+import { BoxFooter } from "./ui/box-footer";
 import { BoxFrame } from "./ui/box-frame";
+import { BoxHeader } from "./ui/box-header";
+import { BoxMain } from "./ui/box-main";
+import { BoxMainContentInput, type AnalyzeResult, type SolveResult } from "./ui/box-main-content-input";
 
-export default function InputForm() {
+interface Props {
+    onAnalyze: (data: AnalyzeResult) => void;
+    onSolve: (data: SolveResult) => void;
+    
+}
+export default function InputForm({onAnalyze, onSolve}: Props) {
 
     
     return (
-        <BoxFrame>
-
-
+        <BoxFrame className="h-full justify-self-center">
+            <BoxHeader title="InputForm.md" />
+                <BoxMain className="pt-20">
+                    <BoxMainContentInput onAnalyze={onAnalyze} onSolve={onSolve} />
+                </BoxMain>
+        
+            <BoxFooter/>
         </BoxFrame>
     )
 }
