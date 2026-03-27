d9f1a0d15030c1edb693dfc84ded6c322998ca9a
DaVid Justin <chaosdudeserad@gmail.com>
2026-03-28 00:08:27 +0800

    updated the routing of the API Call for the calculation; using origins to let CORS have access of the route in the frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,10 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes.logic import router
 
 app = FastAPI(title="Propositional Logic System")
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
