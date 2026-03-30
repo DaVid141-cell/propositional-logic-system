b7ecd25e5df526524e8b6483eb885cb315e53d7f
DaVid Justin <chaosdudeserad@gmail.com>
2026-03-31 02:23:08 +0800

    change the PopUpArrow for the guide component instend

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -1,12 +1,12 @@
+import { Guide } from "../guide";
 import { Navbar } from "../ui/Navbar";
-import { PopUpArrow } from "../ui/Pop-up-arrow";
 
 export function MainLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Navbar/>
                 <div className="flex justify-center">
-                    <PopUpArrow/>
+                    <Guide/>
                 </div>
             <div className="bg-foreground px-10 grid grid-flow-col grid-row-2 gap-20">
                 {children}
