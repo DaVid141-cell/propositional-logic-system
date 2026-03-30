ff00b341ae15e83b5aba29855a83d8516540cfe5
DaVid Justin <chaosdudeserad@gmail.com>
2026-03-31 02:27:12 +0800

    updated the PopUpArrow for better UI

--- frontend/src/components/ui/Pop-up-arrow.tsx
+++ frontend/src/components/ui/Pop-up-arrow.tsx
@@ -1,17 +1,25 @@
-export function PopUpArrow() {
+interface Props {
+    open: boolean;
+}
+export function PopUpArrow({open}: Props) {
     return (
-        <svg className="" width="200" height="150" viewBox="0 0 100 100">
-            <path
-                d="M20 20 L50 30 L80 20
-                M25 30 L50 40 L75 30
-                " 
-                fill="none"
-                stroke="#f34f29"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-        </svg>
+        <div className='flex w-20 h-20 justify-center-safe z-100'>
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                width="84" 
+                height="84" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                stroke="currentColor" 
+                stroke-width="1.25" 
+                stroke-linecap="round" 
+                stroke-linejoin="round" 
+                className={`transition-transform duration-300 ${open ? "rotate-180" : "rotate-0"}`}
+                >
+                <path d="m7 6 5 5 5-5"/>
+                <path d="m7 12 5 5 5-5"/>
+                                
+            </svg>
+        </div>
+        
     );
 }
-
