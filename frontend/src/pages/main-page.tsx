dc743a8f40832633a5ab4b9439db3bf64ac47da7
DaVid Justin <chaosdudeserad@gmail.com>
2026-03-31 02:28:01 +0800

    remove the wrapper for the box-content instend i replace it with the notes component for clarity

--- frontend/src/pages/main-page.tsx
+++ frontend/src/pages/main-page.tsx
@@ -1,12 +1,10 @@
-import {BoxFrame} from "../components/ui/box-frame";
-import { BoxHeader } from "../components/ui/box-header";
-import { BoxMain } from "../components/ui/box-main";
-import { BoxFooter } from "../components/ui/box-footer";
+
 import { MainLayout } from "../components/layout/MainLayout";
 import type { SolveResult, AnalyzeResult } from "../components/ui/box-main-content-input";
 import InputForm from "../components/inputForm";
 import { useState } from "react";
 import Output from "../components/output";
+import { Notes } from "../components/notes";
 
 export default function MainPage() {
   const [analyzeData, setAnalyzeData] = useState<AnalyzeResult | null>(null);
@@ -19,15 +17,8 @@ export default function MainPage() {
 
       <Output analyzeDataOutput={analyzeData} solveDataOutput={solveData} />
       
-
-      <BoxFrame className="row-span-4 h-320">
-        <BoxHeader title="Notes.md" className=""/>
-          <BoxMain className="h-full">
-            <div className="bg-neutral-300 h-full flex items-center justify-center">
-              TEXT
-            </div>  
-          </BoxMain> 
-      </BoxFrame>
+      <Notes/>
+      
 
     </MainLayout>
   );
