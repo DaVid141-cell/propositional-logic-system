a74da0c220be8a7acbe89602d8d42320093c8725
DaVid Justin <chaosdudeserad@gmail.com>
2026-03-31 02:25:36 +0800

    feat: updated the output box UI for better expereince

--- frontend/src/components/ui/box-main-content-output.tsx
+++ frontend/src/components/ui/box-main-content-output.tsx
@@ -9,59 +9,62 @@ export function BoxMainContentOutput({ analyzeData, solveData} : Props) {
         const header = [...analyzeData?.variables || [], "Result"];
 
     return (
-        <div className="p-4 space-y-6 font-mono text-sm">
+      <div className="p-4 space-y-6 text-base">
 
-      {/* Truth Table */}
-      <div>
-        <p className="font-bold mb-2">Truth Table:</p>
-        <table className="border-collapse">
-          <thead>
-            <tr>
-              {header.map((h) => (
-                <th key={h} className="border border-red-500 px-3 py-1">{h}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {analyzeData.truth_table.map((row, i) => (
-              <tr key={i}>
+        {/* Truth Table */}
+        <div>
+          <p className="font-bold mb-2">Truth Table:</p>
+          <table className="border-collapse">
+            <thead>
+              <tr>
                 {header.map((h) => (
-                  <td key={h} className="border border-red-500 px-3 py-1 text-center">
-                    {row.values[h]}
-                  </td>
+                  <th key={h} className="border-b border-r last:border-r-0 px-3 py-1">{h}</th>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
-        
-      </div>
-
-      {/* Steps — only shows if user gave p/q/r values */}
-      {solveData && (
-        <>
-          <div>
-            <p className="font-bold mb-2">Step by step Evaluation:</p>
-            <div className="space-y-1">
-              {solveData.steps.map((step, i) => (
-                <p key={i} className="opacity-70">{step}</p>
+            </thead>
+            <tbody>
+              {analyzeData.truth_table.map((row, i) => (
+                <tr className="border-b last:border-b-0  hover:bg-neutral-700 transition-colors" key={i}>
+                  {header.map((h) => (
+                    <td key={h} className="border-r last:border-r-0  px-3 py-1 text-center">
+                      <span className={row.values[h] === "T" ? "text-green-400" : "text-red-500"}>
+                        {row.values[h]}
+                        </span>
+                    </td>
+                  ))}
+                </tr>
               ))}
-            </div>
-          </div>
-
-          <div>
-            <p className="font-bold mb-2">Final result of the given statement:</p>
-            <p className="opacity-70">
-              {analyzeData.expression} = {solveData.result}
-            </p>
-            
-            {/*Anlysis */}
-            <p className="mt-3 opacity-60">Analysis: {analyzeData.analysis}</p>
+            </tbody>
+          </table>
           
-          </div>
-        </>
-      )}
+        </div>
 
+        {/* Steps — only shows if user gave p/q/r values */}
+        {solveData && (
+          <>
+            <div>
+              <p className="font-bold mb-2">Step by step Evaluation:</p>
+              <div className="space-y-1">
+                {solveData.steps.map((step, i) => (
+                  <p key={i} className="opacity-70">{step}</p>
+                ))}
+              </div>
+            </div>
+
+            <div>
+              <p className="font-bold mb-2">Final result of the given statement:</p>
+              <p className="opacity-80">
+                {analyzeData.expression} = {solveData.result}
+              </p>
+              
+              {/*Anlysis */}
+              <p className="mt-3 text-base">Analysis: 
+                <span className="text-red-500 font-bold"> {analyzeData.analysis}</span>
+              </p>
+            
+            </div>
+          </>
+        )}
     </div>
     )
 }
