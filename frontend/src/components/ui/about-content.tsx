0f81e6d53f507d57110e3d749617cffbcb37d56f
DaVid Justin <chaosdudeserad@gmail.com>
2026-03-31 22:49:30 +0800

    added the contents for the about apge

---
+++ frontend/src/components/ui/about-content.tsx
@@ -0,0 +1,33 @@
+export function AboutContent () {
+    return (
+        <div className="flex bg-accent-foreground backdrop-blur-md shadow-lg">
+            <div className="p-10 space-y-6 text-justify">
+                <h3>Hello everyone</h3>
+                <p>
+                    I would like to share this small application that I created to help solve problems related to propositional logic.
+                </p>
+
+                <p>
+                    The idea for this project actually came from a simple moment of boredom. While I was answering on a quiz for one of my subjects, I started thinking about how interesting it would be to build a small system that could evaluate logical statements automatically. And out of curiosity and honesty, i was just trying to pass the time and then i began experimenting with the idea, and before I knew it, I had started developing this application.
+                </p>
+
+                <p>
+                    To be honest, I didn’t expect it to work as well as it does. Unfortunately, by the time I finished building most of the features, the specific topic in our class had already moved on. Because of that, I didn’t get to use the application as much as I originally intended. Still, it was a fun and rewarding experience to see everything come together and function properly.
+                </p>
+
+                <p>
+                    It’s interesting how something as simple as boredom can turn into a creative opportunity and lead to building something useful and educational.
+                </p>
+
+                <p>
+                    If you enjoy using this application, I would truly appreciate your feedback, about your thoughts whether positive or negative it can help improve the system in the future. Hearing how you guys interact with the application and what you guys think about it, will surely help guide future updates and improvements.
+                </p>
+
+                <p>
+                    Thank you very much for taking the time to try out this project. It was genuinely fun to build, and I hope you find it helpful as well.
+                </p>
+
+            </div>
+        </div>
+    )
+}
