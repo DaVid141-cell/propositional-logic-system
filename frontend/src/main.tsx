6c585ec4d324b1e1923f3278e32af08059ecf4a8
DaVid Justin <chaosdudeserad@gmail.com>
2026-03-31 22:48:05 +0800

    feat: added the routing for each of the pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainPage from './pages/main-page'
+import AboutPage from './pages/about'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MainPage/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
