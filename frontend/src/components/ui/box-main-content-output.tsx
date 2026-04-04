09c98184a7912c9ccc15d3adc1d97e80392a237e
DaVid Justin <chaosdudeserad@gmail.com>
2026-04-04 17:32:27 +0800

    fixed the issue with the mapping of the truthtable

--- frontend/src/components/ui/box-main-content-output.tsx
+++ frontend/src/components/ui/box-main-content-output.tsx
@@ -1,9 +1,11 @@
-import type { AnalyzeResult, SolveResult } from "./box-main-content-input";
+import type { AnalyzeResult, SolveResult, TruthTableRow } from "./box-main-content-input";
 
 interface Props {
     analyzeData: AnalyzeResult;
     solveData: SolveResult | null;
 }
+
+
 
 export function BoxMainContentOutput({ analyzeData, solveData} : Props) {
         const header = [...analyzeData?.variables || [], "Result"];
@@ -23,12 +25,12 @@ export function BoxMainContentOutput({ analyzeData, solveData} : Props) {
               </tr>
             </thead>
             <tbody>
-              {analyzeData.truth_table.map((row, i) => (
+              {analyzeData.truth_table.map((row: TruthTableRow, i: number) => (
                 <tr className="border-b last:border-b-0  hover:bg-neutral-700 transition-colors" key={i}>
                   {header.map((h) => (
                     <td key={h} className="border-r last:border-r-0  px-3 py-1 text-center">
-                      <span className={row.values[h] === "T" ? "text-green-400" : "text-red-500"}>
-                        {row.values[h]}
+                      <span className={row.values[h] ? "text-green-400" : "text-red-500"}>
+                        {row.values[h] ? "T" : "F"}
                         </span>
                     </td>
                   ))}
