c5b58cad45ff20fa259e8fa4a0e2a745af3ea2b9
DaVid Justin <chaosdudeserad@gmail.com>
2026-04-25 20:39:47 +0800

    fix: add Analytics component to main routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainPage from './pages/main-page'
 import AboutPage from './pages/about'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Analytics/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
